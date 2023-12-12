7f2bdc9c17bf433a6c9dbfa0da00e128ddb2821d
Tyler Bush <tyler.bush.1@slu.edu>
2023-12-12 11:27:35 -0600

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+    if (numOfTimes < 0) return "ERROR";
+    let repeatedString = "";
+    for (let num = 0; num < numOfTimes; num++){
+        repeatedString += string;
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
