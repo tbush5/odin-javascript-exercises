22316cb1e845fe4cc2424ef51078b3f8e0596ab8
Tyler Bush <tyler.bush.1@slu.edu>
2023-12-15 14:34:40 -0600

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    if (stop < start){
+        let temp = start;
+        start = stop;
+        stop = temp;
+    }
+    if (start < 0 || typeof(start) != "number" || typeof(stop) != "number"){
+        return "ERROR";
+    }
+    let ans = 0;
+    for (let i=start; i <= stop; i++){
+        ans += i
+    }
+    return ans;
 };
 
 // Do not edit below this line
